771150f5aefc0321a083ddad0fb3464e16d9d27c
Emily Hudson <emily33901@gmail.com>
2024-04-27 22:47:35 +0100

    cargo fmt

--- src/texture.rs
+++ src/texture.rs
@@ -1,27 +1,19 @@
 // This file contains implementations inspired by or derived from the following sources:
 // - https://github.com/ohchase/egui-directx/blob/master/egui-directx11/src/texture.rs
-// 
+//
 // Here I would express my gratitude for their contributions to the Rust community.
 // Their work served as a valuable reference and inspiration for this project.
-// 
+//
 // Nekomaru, March 2024
 
 use std::collections::HashMap;
 use std::mem;
 use std::slice;
 
-use egui::{
-    Color32,
-    ImageData,
-    TextureId,
-    TexturesDelta,
-};
+use egui::{Color32, ImageData, TextureId, TexturesDelta};
 
 use windows::core::Result;
-use windows::Win32::Graphics::{
-    Dxgi::Common::*,
-    Direct3D11::*,
-};
+use windows::Win32::Graphics::{Direct3D11::*, Dxgi::Common::*};
 
 struct Texture {
     tex: ID3D11Texture2D,
@@ -36,24 +28,22 @@ pub struct TexturePool {
 }
 
 impl TexturePool {
-    pub fn new(device: &ID3D11Device)-> Self {
+    pub fn new(device: &ID3D11Device) -> Self {
         Self {
             device: device.clone(),
             pool: HashMap::new(),
         }
     }
 
-    pub fn get_srv(&self, tid: TextureId)-> Option<ID3D11ShaderResourceView> {
+    pub fn get_srv(&self, tid: TextureId) -> Option<ID3D11ShaderResourceView> {
         self.pool.get(&tid).map(|t| t.srv.clone())
     }
 
-    pub fn update(
-        &mut self,
-        ctx: &ID3D11DeviceContext,
-        delta: TexturesDelta)-> Result<()> {
+    pub fn update(&mut self, ctx: &ID3D11DeviceContext, delta: TexturesDelta) -> Result<()> {
         for (tid, delta) in delta.set {
             if delta.is_whole() {
-                self.pool.insert(tid, Self::create_texture(&self.device, delta.image)?);
+                self.pool
+                    .insert(tid, Self::create_texture(&self.device, delta.image)?);
                 // the old texture is returned and dropped here, freeing
                 // all its gpu resource.
             } else if let Some(tex) = self.pool.get_mut(&tid) {
@@ -73,27 +63,20 @@ impl TexturePool {
         old: &mut Texture,
         image: ImageData,
         [nx, ny]: [usize; 2],
-    )-> Result<()> {
+    ) -> Result<()> {
         let subr = unsafe {
             let mut output = D3D11_MAPPED_SUBRESOURCE::default();
-            ctx.Map(
-                &old.tex,
-                0,
-                D3D11_MAP_WRITE_DISCARD,
-                0,
-                Some(&mut output),
-            )?;
+            ctx.Map(&old.tex, 0, D3D11_MAP_WRITE_DISCARD, 0, Some(&mut output))?;
             output
         };
         match image {
             ImageData::Font(f) => {
                 let data = unsafe {
-                    let slice = slice::from_raw_parts_mut(
-                        subr.pData as *mut Color32,
-                        old.pixels.len());
-                    slice.as_mut_ptr().copy_from_nonoverlapping(
-                        old.pixels.as_ptr(),
-                        old.pixels.len());
+                    let slice =
+                        slice::from_raw_parts_mut(subr.pData as *mut Color32, old.pixels.len());
+                    slice
+                        .as_mut_ptr()
+                        .copy_from_nonoverlapping(old.pixels.as_ptr(), old.pixels.len());
                     slice
                 };
 
@@ -111,17 +94,14 @@ impl TexturePool {
                         data[whole] = new[frac];
                     }
                 }
-            },
+            }
             _ => unreachable!(),
         }
         unsafe { ctx.Unmap(&old.tex, 0) };
         Ok(())
     }
 
-    fn create_texture(
-        device: &ID3D11Device,
-        data: ImageData,
-    )-> Result<Texture> {
+    fn create_texture(device: &ID3D11Device, data: ImageData) -> Result<Texture> {
         let width = data.width();
 
         let pixels = match &data {
@@ -156,19 +136,11 @@ impl TexturePool {
         };
 
         let mut tex = None;
-        unsafe { device.CreateTexture2D(
-            &desc,
-            Some(&subresource_data),
-            Some(&mut tex))
-        }?;
+        unsafe { device.CreateTexture2D(&desc, Some(&subresource_data), Some(&mut tex)) }?;
         let tex = tex.unwrap();
 
         let mut srv = None;
-        unsafe { device.CreateShaderResourceView(
-            &tex,
-            None,
-            Some(&mut srv))
-        }?;
+        unsafe { device.CreateShaderResourceView(&tex, None, Some(&mut srv)) }?;
         let srv = srv.unwrap();
 
         Ok(Texture {
